84a6f4be1d327a76939d5b9090cf8fc245b4fd9f
blahova <118811455+blahova@users.noreply.github.com>
2022-12-09 11:33:56 +0100

    upravene

--- postfix.py
+++ postfix.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 def eval_expr(s):
-    s=s.split()
-    z=[]    #zasobnik
-    while(len(s) != 0):
-        while(s[0].isdigit()):
-            z.append(s.pop(0))
-        if (s[0]=="/"):
-            z.append(str(eval(z.pop(0)+s.pop(0)*2+z.pop(0))))
-        else:
-            z.append(str(eval(z.pop(0)+s.pop(0)+z.pop(0))))
-    return int(z[0])
-
+        s=s.split()
+        z=[]    #zasobnik
+        while(len(s) != 0):
+                while(s[0].isdigit()):
+                        z.append(s.pop(0))
+                if (s[0]=="/"):
+                        z.append(str(eval(z.pop(-2)+s.pop(0)*2+z.pop())))
+                else:
+                        z.append(str(eval(z.pop(-2)+s.pop(0)+z.pop())))
+        return int(z[0])
