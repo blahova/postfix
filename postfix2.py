4a04a25e52dde8f23a622c773e2dd3500b4d8246
blahova <118811455+blahova@users.noreply.github.com>
2022-12-11 17:35:09 +0100

    opravene if/elif

--- postfix2.py
+++ postfix2.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 def eval_expr(s):
         s=s.split()
-        z=[]    #zasobnik
-        while(len(s) != 0):
-                while(s[0].isdigit()):
-                        z.append(int(s.pop(0)))
-                if (s[0]=="+"):
-                        z.append(z.pop(-2)+z.pop())
-                elif (s[0]=="-"):
-                        z.append(z.pop(-2)-z.pop())
-                elif (s[0]=="*"):
-                        z.append(z.pop(-2)*z.pop())
-                elif (s[0]=="/"):
-                        z.append(z.pop(-2)//z.pop())
+	z=[]	#zasobnik
+	while(len(s) != 0):
+		if (s[0]=="+"):
+			z.append(z.pop(-2)+z.pop())
+		elif (s[0]=="-"):
+			z.append(z.pop(-2)-z.pop())
+		elif (s[0]=="*"):
+			z.append(z.pop(-2)*z.pop())
+		elif (s[0]=="/"):
+			z.append(z.pop(-2)//z.pop())
+		else:
+			z.append(int(s[0]))
                 s.pop(0)
-        return z[0]
+	return z[0]
